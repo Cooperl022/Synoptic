ca2f35abd2bcb6e3f7be4364b27d99836519526c
Matthew-033 <117305230+Matthew-033@users.noreply.github.com>
2023-06-03 22:04:21 +0100

    Forgot a few things
    Forgot to combine in the toggle buttons and previous version was missing add stock stuff

--- Synoptic 2023/routes/myBusiness.js
+++ Synoptic 2023/routes/myBusiness.js
@@ -24,4 +24,24 @@ router.get('/', function(req, res, next) {
     }
 });
 
+/* Add stock */
+router.post('/', function (req, res, next) {
+    var sql = 'select * from foodPlaces where place_owner = ?'
+    synopticModel.query(sql, [req.session.username], function (error, infoResult) {
+        if (error) throw error;
+        const place_name = infoResult[0].place_name;
+    
+    const item_name = req.body.ItemName
+    const item_description = req.body.ItemDesc
+    const expiry = req.body.ExpiryDate
+    const quantity = req.body.Quantity
+
+    var sql = 'insert into stock (place_name, item_name, item_description, expiry, quantity) VALUES (?, ?, ?, ?, ?)';
+    synopticModel.query(sql, [place_name, item_name, item_description, expiry, quantity], function (error, result) {
+      if (error) throw error;
+      res.redirect('back')
+    }); 
+    });
+  });
+
 module.exports = router;
